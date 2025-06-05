df08ccb6640311ce71b8e93e3eb82686ff59dab3
StephanieJohansson <3tomu24.johast@folkuniversitetet.nu>
2025-06-05 17:47:49 +0200

    made resttemplate as a bean in main app

--- src/main/java/com/example/EVCharging/EvChargingApplication.java
+++ src/main/java/com/example/EVCharging/EvChargingApplication.java
@@ -2,6 +2,8 @@ package com.example.EVCharging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EvChargingApplication {
@@ -10,4 +12,8 @@ public class EvChargingApplication {
 		SpringApplication.run(EvChargingApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
