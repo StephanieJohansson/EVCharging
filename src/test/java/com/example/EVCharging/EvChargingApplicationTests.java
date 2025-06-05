72b3dc73c2ac1f33cb19d97c6534b8441f5f7a84
StephanieJohansson <3tomu24.johast@folkuniversitetet.nu>
2025-06-05 10:47:07 +0200

    first commit

---
+++ src/test/java/com/example/EVCharging/EvChargingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EVCharging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
